6f64e492cd173674a358874eb901db250bfb3657
mark-renrgyx <53844281+mark-renrgyx@users.noreply.github.com>
2019-08-08 09:41:06 -0400

    finished -- the easy way, enough to demonstrate control loops

---
+++ Rangoli.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+// Assignment 2.2
+
+/**
+ * I *think* this is Rangoli
+ * @author mark
+ *
+ */
+public class Rangoli {
+
+	public static void main(String[] args) {
+		
+		String lines [] = {"    a    ", "  a b a  ", "a b c b a", "  a b a  ", "    a    "};
+		
+		for (int i = 0; i < lines.length; i++) {
+			System.out.println(lines[i]);
+		}
+	}
+}
